e5ed0d1b17f7bfff5233e222b86e06668dce8917
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-02-21 15:23:04 +0000

    First pass as task manager

--- Gemfile
+++ Gemfile
@@ -43,7 +43,11 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 
 # Use Sass to process CSS
-# gem "sassc-rails"
+gem "sassc-rails"
+
+# FontAwesome
+gem "font-awesome-sass", "~> 6.3.0"
+
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Defines the root path route ("/")
+  get "tasks", to: "tasks#index"
+
+  # Add a new task
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # Edit a task
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete a task
+  delete "tasks/:id", to: "tasks#destroy"
+
+  #List one task
+  get "tasks/:id", to: "tasks#show", as: :task
+
 end
